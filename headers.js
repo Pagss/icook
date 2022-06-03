edd994878087fc6a316509c3ad852e310f23093b
Rodrigo Pagano <rpdev22@gmail.com>
2022-06-03 19:18:44 -0300

    content security policy

--- headers.js
+++ headers.js
@@ -31,8 +31,8 @@ module.exports = [
     key: "Permissions-Policy",
     value: "geolocation=*", // allow specified policies here
   },
-  // {
-  //   key: "Content-Security-Policy",
-  //   value: "default-src 'self'",
-  // },
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self' 'unsafe-eval' 'unsafe-inline'",
+  },
 ];
